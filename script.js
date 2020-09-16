1c1cf9e9748a2ac97f18102374581c06ac5bfcf4
Mariusz Wojtkiewicz <wojtkiewicz89@gmail.com>
2020-09-16 19:35:48 +0200

    add remove tasks

--- script.js
+++ script.js
@@ -10,6 +10,21 @@
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
+
+
     const render = () => {
         let htmlString = "";
 
@@ -18,21 +33,21 @@
             <li
             ${task.done ? " style=\"text-decoration: line-through\"" : ""}
             >
+            <button class="js-remove">Usuń zadanie</button>
             ${task.content}
             </li>
             `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
 
+        const removeButtons = document.querySelectorAll(".js-remove");
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
         });
-
-        render();
     };
 
     const onFormSubmit = (event) => {
@@ -46,7 +61,7 @@
         }
 
         addNewTask(newTaskContent);
-    }
+    };
 
     const init = () => {
         render();
